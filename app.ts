e84fb57787154bebc0588ed6634ada38e80d5b76
Nattie C. <47332368+nattiechan@users.noreply.github.com>
2025-02-19 14:07:50 -0800

    feat: clean up app.ts
    - Add a more distinct message for
    `/pug` endpoint
    - We can pass ENV variable in
    via docker compose so we don't
    need dotenv or `.env` file anymore
    - Minor linting changes

--- app.ts
+++ app.ts
@@ -1,30 +1,29 @@
-import express, { NextFunction, Request, Response } from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-
-const isLocal = process.env.IS_LOCAL === "true";
-
-if (isLocal) {
-  dotenv.config();
-}
+import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Health check endpoint
-app.get("/", (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.sendStatus(200);
 });
 
-app.get("/pug", (req: Request, res: Response) => {
+app.get('/pug', (req: Request, res: Response) => {
+  // This can be handy in checking whether the DBNAME ENV variable
+  // is passed into the server successfully
   console.log(process.env.DBNAME);
-  res.sendStatus(200);
+  res
+    .status(200)
+    .send(
+      'Did you know that Tibetan monks kept pugs as pets and may have helped standardize the breed?'
+    );
 });
 
 // 404 Error Handler
 app.use((req: Request, res: Response) => {
-  res.status(404).json({ error: "Not Found" });
+  res.status(404).json({ error: 'Not Found' });
 });
 
 const port = process.env.PORT || 3000;
